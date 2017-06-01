ddfab3f38a10ee57f8abfc87e954e5f352e582d2
Maro <grafika.marekduda@gmail.com>
2017-06-01 21:12:55 +0200

    Added space between characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var a = 5;
 var h = 10;
-var triangleArea = a*h/2;
+var triangleArea = a * h / 2;
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
